dc81601322a848e1cec7abe76e42b57e9d37eb0e
wanderfranca <wander.wpf@hotmail.com>
2022-06-29 16:46:03 -0300

    criado o model e migration de grupos_usuarios

--- app/Database/Migrations/2022-06-29-193808_CriaTabelaGruposUsuarios.php
+++ app/Database/Migrations/2022-06-29-193808_CriaTabelaGruposUsuarios.php
@@ -8,11 +8,34 @@ class CriaTabelaGruposUsuarios extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id'          => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'grupo_id'          => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+            ],
+            'usuario_id'          => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+            ],
+
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('grupo_id','grupos','id','CASCADE','CASCADE');
+        $this->forge->addForeignKey('usuario_id','usuarios','id','CASCADE','CASCADE');
+        $this->forge->createTable('grupos_usuarios');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('grupos_usuarios');
     }
 }
---
+++ app/Models/GrupoUsuarioModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class GrupoUsuarioModel extends Model
+{
+    protected $table            = 'grupos_usuarios';
+    protected $returnType       = 'object';
+    protected $allowedFields    = ['grupo_id', 'usuario_id'];
+
+}

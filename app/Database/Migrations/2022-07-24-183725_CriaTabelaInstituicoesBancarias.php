8e7e97a67302543d6f40afc2d7776239ff39a3d8
wanderfranca <wander.wpf@hotmail.com>
2022-08-31 20:35:45 -0300

    upd correcao da tabela migration e o migrate CriaTabelaInstituicoesBancarias

--- app/Database/Migrations/2022-07-24-183725_CriaTabelaInstituicoesBancarias.php
+++ app/Database/Migrations/2022-07-24-183725_CriaTabelaInstituicoesBancarias.php
@@ -4,7 +4,7 @@ namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
 
-class CriaTabelaTiposDeDocumentos extends Migration
+class CriaTabelaInstituicoesBancarias extends Migration
 {
     public function up()
     {

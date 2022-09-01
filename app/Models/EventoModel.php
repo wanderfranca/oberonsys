a838bf43c9cccf8421cda051e178993caafc0587
wanderfranca <wander.wpf@hotmail.com>
2022-09-01 19:07:07 -0300

    237.02

--- app/Models/EventoModel.php
+++ app/Models/EventoModel.php
@@ -6,37 +6,49 @@ use CodeIgniter\Model;
 
 class EventoModel extends Model
 {
-    protected $DBGroup          = 'default';
     protected $table            = 'eventos';
-    protected $primaryKey       = 'id';
-    protected $useAutoIncrement = true;
-    protected $insertID         = 0;
     protected $returnType       = 'array';
-    protected $useSoftDeletes   = false;
-    protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = [
+        'contapagar_id',
+        'ordem_id',
+        'title',
+        'start',
+        'end',
+    ];
 
     // Dates
-    protected $useTimestamps = false;
-    protected $dateFormat    = 'datetime';
+    protected $useTimestamps = true;
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
 
-    // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
+    public function recuperaEventos(array $dataGet)
+    {
+        return $this->where('start >', $dataGet['start'])
+                    ->where('end <', $dataGet['end'])
+                    ->findAll();
+    }
 
-    // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
+    public function cadastraEvento(string $coluna, string $titulo, int $id, int $dias)
+    {
+        $evento = [
+            "$coluna"   => $id,
+            "title"     => $titulo,
+            "start"     => date("Y-m-d", strtotime("+$dias days", time())),
+            "end"       => date("Y-m-d", strtotime("+$dias days", time())),
+
+        ];
+
+        return $this->insert($evento);
+    }
+
+
+    public function atualizaEvento(string $coluna, int $id, int $dias)
+    {
+        return $this->protected(false)
+                    ->where($coluna, $id)
+                    ->set('start', date("Y-m-d", strtotime("+$dias days", time())))
+                    ->set('end', date("Y-m-d", strtotime("+$dias days", time())))
+                    ->update();
+    }
+    
 }
